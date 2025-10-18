178b4966fb2c9f0adc85d32d2f97d75a7df68403
Modinat1 <adenikem@yahoo.com>
2025-10-18 10:29:13 +0100

    refactor: streamline user profile response structure

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,19 +5,15 @@ const getUserProfile = async (req, res) => {
     const fact = await getCatFact();
     const timestamp = new Date().toISOString();
 
-    const user = {
-      email: "modinat@gmail.com",
-      name: "Modinat",
-      stack: "Node.js/Express",
-      fact: fact,
-    };
     return res.status(200).json({
-      message: "User found successfully!",
-      data: {
-        status: "success",
-        user: user,
-        timestamp,
+      status: "success",
+      user: {
+        email: "modinat@gmail.com",
+        name: "Modinat",
+        stack: "Node.js/Express",
       },
+      timestamp,
+      fact: fact,
     });
   } catch (error) {
     return res.status(500).json({
